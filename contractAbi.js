b1c535d63c46d386cc3ef474b8b58feecbcee352
Andre Zimin <130975402+andrzimin72@users.noreply.github.com>
2025-06-19 09:44:06 +0800

    Delete contractAbi.js

--- contractAbi.js
+++
@@ -1,5 +0,0 @@
-import abi from '../abi/SimpleVoting.json' assert { type: 'json' };
-
-const contractAddress = "YOUR_CONTRACT_ADDRESS"; // Replace with deployed address
-
-export { abi, contractAddress };
